c9c704d8246f95838bc63e8be745652e6a175e8e
Robert Gudat <robert.gudat@web.de>
2021-07-23 23:17:17 +0200

    Corrected git ignore.

--- build/ExampleMiddleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExampleMiddleware = void 0;
-let ExampleMiddleware = function (request, response, next) {
-    response.writeStatus("202 Accepted");
-    next(request, response, next);
-};
-exports.ExampleMiddleware = ExampleMiddleware;
--- build/RequestData.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class RequestData {
-    constructor(headers, data) {
-        this.headers = headers;
-        this.data = data;
-    }
-}
-exports.default = RequestData;
