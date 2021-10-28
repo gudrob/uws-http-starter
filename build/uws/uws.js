a4a4f73e84f79f000a4399daec0a78d6adc8ce04
Robert Gudat <robert.gudat@hutter-unger.de>
2021-10-28 14:49:41 +0200

    Removed uws build files.

--- build/uws/uws.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = (() => {
-    try {
-        return require('./uws_' + process.platform + '_' + process.arch + '_' + process.versions.modules + '.node');
-    }
-    catch (e) {
-        throw new Error('This version of µWS is not compatible with your Node.js build:\n\n' + e.toString());
-    }
-})();
